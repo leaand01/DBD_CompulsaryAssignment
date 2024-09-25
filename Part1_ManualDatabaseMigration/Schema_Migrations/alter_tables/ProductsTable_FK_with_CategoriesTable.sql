e381ca8a0184d596126401efc79d0a36d6246bf2
leaand01 <leaand01@easv365.dk>
2024-09-25 20:05:49 +0200

    feat(Products)!: add Category_Id coolumn to Products table and make it a FK
    BREAKING CHANGE: Category must be not null as is a foreign key refering to the Categories table

---
+++ Part1_ManualDatabaseMigration/Schema_Migrations/alter_tables/ProductsTable_FK_with_CategoriesTable.sql
@@ -0,0 +1,16 @@
+use [e-commerce];
+
+-- add new column to Products table
+alter table Products
+add Category_Id int; -- In case Products table alreade have data cannot specify not null here.
+go
+
+alter table Products
+alter column Category_Id int not null; -- add null constraint here instead
+go
+
+-- Setup foreign key between Products and Categories tables on column Category_Id
+alter table Products
+add constraint fk_category
+foreign key (Category_Id) references Categories(Category_Id);
+go
